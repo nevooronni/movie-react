2458e1aa019d42a194a0f4f6337107a7a80790ef
nevooronni <nevooronni@gmail.com>
2018-06-24 22:24:14 +0300

    added new movies features

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -3,15 +3,27 @@ import React, { Component } from 'react';
 class AddMovie extends Component {
     render() {
         return (
-            <form className="movie-form">
+            <form className="movie-form" onSubmit={(e) => this.addNewMovie(e)}>
                 <p>Add a Movie</p>
                 <input ref={ ( input ) => this.title = input } type="input" placeholder="Title" />
-                <input ref={ ( input ) => this.yeaer = input } type="text" placeholder="Year" />
+                <input ref={ ( input ) => this.year = input } type="text" placeholder="Year" />
                 <input ref={ ( input ) => this.poster = input } type="text" placeholder="Poster" />
-                <textarea ref={ ( input ) => this.description } placeholder="Description"></textarea>
+                <textarea ref={ ( input ) => this.description = input } placeholder="Description"></textarea>
                 <button type="submit">Add Movie</button>
             </form>
         );
+    }
+
+    addNewMovie(e) {
+        e.preventDefault();
+        //create a new movie object by grabbing the form input field values we stored as component class properties earlier
+        var movie = {
+            title: this.title.value,
+            year: this.year.value,
+            poster: this.poster.value,
+            description: this.description.value   
+        };
+        this.props.addMovie( movie );
     }
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,11 @@ class App extends Component {
 
     //bind the events in react in order for our component methods to work
     this.loadAdditionalMovies = this.loadAdditionalMovies.bind(this);
+
+    //bind the new method to this so it's available throughour the class
+    this.addMovieToGallery = this.addMovieToGallery.bind( this );
   }
+
   render() {
     return (
       <div className="App">
@@ -38,15 +42,35 @@ class App extends Component {
           }
         </div>
         <div className="add-movies"><button onClick={this.loadAdditionalMovies}>Load more...</button></div>      
-        <AddMovie />
+        <AddMovie addMovie={this.addMovieToGallery}/>
       </div>
     );
   }
 
+  //load additional movies
   loadAdditionalMovies() {
     var currentMovies = { ...this.state.movies };
     var newMovies = Object.assign( currentMovies, additionalMovies );
    
+    this.setState({ movies: newMovies });
+  }
+
+  //method to add new movies to the movie state object
+  addMovieToGallery( movie ) {
+    console.log( 'movie', movie );
+    //add the movie details to the movies state object
+    
+    //set date
+    var ts = Date.now();
+    //new movie object
+    var newMovie = {};
+    //add the movie and time
+    newMovie[ 'movie' + ts ] = movie;
+    //get current movies
+    var currentMovies = { ...this.state.movies };
+    //assign all this to the currentmovies
+    var newMovies = Object.assign( currentMovies, newMovie );
+    //set state to this new bunch of movies
     this.setState({ movies: newMovies });
   }
 
